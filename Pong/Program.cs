5702ed33ba0e439714b817109b47385ecc0e5559
LWY <668915+laxa88@users.noreply.github.com>
2022-07-23 21:02:33 +0900

    add ball movement logic, lint with csharpier

--- Pong/Program.cs
+++ Pong/Program.cs
@@ -2,13 +2,13 @@
 
 namespace Pong
 {
-  public static class Program
-  {
-    [STAThread]
-    static void Main()
+    public static class Program
     {
-      using (var game = new Game1())
-        game.Run();
+        [STAThread]
+        static void Main()
+        {
+            using (var game = new Game1())
+                game.Run();
+        }
     }
-  }
 }
