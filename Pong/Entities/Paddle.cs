c1ab2c893f88c393d1ab27ec3c8e45c69dcafac7
LWY <668915+laxa88@users.noreply.github.com>
2022-08-07 22:14:06 +0900

    remove unused code

--- Pong/Entities/Paddle.cs
+++ Pong/Entities/Paddle.cs
@@ -1,7 +1,5 @@
-using System;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
-using Microsoft.Xna.Framework.Input;
 
 namespace Pong
 {
@@ -20,9 +18,6 @@ namespace Pong
         {
             get { return _drawRect.Height; }
         }
-
-        public event EventHandler OnMoveUp;
-        public event EventHandler OnMoveDown;
 
         public Paddle(Game game, GraphicsDeviceManager graphics, SpriteBatch spriteBatch)
             : base(game, graphics, spriteBatch) { }
